3e101f136ff27ea184ceca1529f3a07aece947d8
Ryan Woods <contact@ryanofwoods.com>
2021-08-10 17:28:04 +0200

    Add StaticPagesController and homepage
    Contains ReceiptManager information as well as a random cat image.
    
    Includes testing.

---
+++ test/integration/static_pages_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class StaticPagesTest < ActionDispatch::IntegrationTest
+  test 'homepage should have a random cat image' do
+    get root_path
+
+    valid_image_from_catapi_regexp = /catapi.+\.(jpg|png)/
+    assert_select 'img', src: valid_image_from_catapi_regexp, count: 1
+  end
+end
