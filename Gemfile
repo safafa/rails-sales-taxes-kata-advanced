27718ad3f7bc0e6d68260950e134c478e8873c43
Ryan Woods <contact@ryanofwoods.com>
2021-08-11 13:56:20 +0200

    Add devise and User model
    Note, the devise User has been added as :confirmable.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    post '/login', to: 'devise/sessions#create'
+    delete '/logout', to: 'devise/sessions#destroy'
+  end
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
