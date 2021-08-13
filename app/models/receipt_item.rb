c36e4b36a6a22589d96b8077a47b208618ac2656
Ryan Woods <contact@ryanofwoods.com>
2021-08-13 15:53:43 +0200

    Add ReceiptItem logic with tests

--- app/models/receipt_item.rb
+++ app/models/receipt_item.rb
@@ -2,9 +2,23 @@
 
 class ReceiptItem < ApplicationRecord
   include ReceiptManager
+  include TaxHandler
 
   belongs_to :receipt
   belongs_to :product, class_name: PRODUCT_CLASS_NAME
+
   validates_presence_of :quantity, :name, :price
   validates :imported, exclusion: [nil]
+
+  def category
+    product.category
+  end
+
+  def sales_tax
+    @sales_tax ||= calculate_sales_tax(self)
+  end
+
+  def shelf_price
+    @shelf_price ||= (price * quantity + sales_tax).round(2)
+  end
 end
---
+++ test/models/receipt_item_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ReceiptItemTest < ActiveSupport::TestCase
+  test 'returns correct shelf price and sales tax for items with no sales tax' do
+    item = ReceiptItem.new(quantity: 1, price: 12.49, imported: false, product: products(:book))
+
+    shelf_price = item.shelf_price
+    sales_tax = item.sales_tax
+    assert_equal shelf_price, 12.49
+    assert_equal sales_tax, 0
+  end
+
+  test 'returns correct shelf price and sales tax for items with only basic sales tax' do
+    item = ReceiptItem.new(quantity: 1, price: 14.99, imported: false, product: products(:music_cd))
+
+    shelf_price = item.shelf_price
+    sales_tax = item.sales_tax
+    assert_equal shelf_price, 16.49
+    assert_equal sales_tax, 1.50
+  end
+
+  test 'returns correct shelf price and sales tax for items with only import tax' do
+    item = ReceiptItem.new(quantity: 1, price: 10.00, imported: true, product: products(:chocolate))
+
+    shelf_price = item.shelf_price
+    sales_tax = item.sales_tax
+    assert_equal shelf_price, 10.50
+    assert_equal sales_tax, 0.50
+  end
+
+  test 'returns correct shelf price and sales tax for items with basic sales tax and import tax' do
+    item = ReceiptItem.new(quantity: 1, price: 47.50, imported: true, product: products(:music_cd))
+
+    shelf_price = item.shelf_price
+    sales_tax = item.sales_tax
+    assert_equal shelf_price, 54.65
+    assert_equal sales_tax, 7.15
+  end
+end
