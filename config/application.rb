f75ec7e49282ce6c1a9cd0723ceffec1ab5c4a5e
Ryan Woods <contact@ryanofwoods.com>
2021-08-13 15:57:44 +0200

    Make lib modules accessible to all models

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module RailsSalesTaxesKataAdvanced
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    # Load lib modules so they are automatically accessible to models
+    config.eager_load_paths += %W[#{config.root}/lib]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
