057301cdb06a0ced70be22564eb674add0e1ee8c
Ryan Woods <contact@ryanofwoods.com>
2021-08-13 15:41:09 +0200

    Add Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_112633) do
+ActiveRecord::Schema.define(version: 2021_08_12_125907) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
