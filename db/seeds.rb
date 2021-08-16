a18a3af6e51edc12054ecde05dc1c14fdb2bfd51
Ryan Woods <contact@ryanofwoods.com>
2021-08-16 18:12:33 +0200

    Add seeds
    For testing around in development or a test production.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+books = Category.find_or_create_by(name: 'book')
+food = Category.find_or_create_by(name: 'food')
+medical = Category.find_or_create_by(name: 'medical')
+other = Category.find_or_create_by(name: 'other')
+
+Product.find_or_create_by(name: 'book', category_id: books.id)
+Product.find_or_create_by(name: 'music CD', category_id: other.id)
+Product.find_or_create_by(name: 'chocolate bar', category_id: food.id)
+Product.find_or_create_by(name: 'box of chocolates', category_id: food.id)
+Product.find_or_create_by(name: 'bottle of perfume', category_id: other.id)
+Product.find_or_create_by(name: 'packet of headache pills', category_id: medical.id)
