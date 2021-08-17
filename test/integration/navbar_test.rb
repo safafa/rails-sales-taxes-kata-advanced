1ce255de30d0bad0a1b97c97be3ed5846c022eeb
Ryan Woods <contact@ryanofwoods.com>
2021-08-17 10:42:47 +0200

    Add Receipt links to navbar
    If you are logged in, the navbar will now provide links to upload a
    receipt or to see all your saved/uploaded receipts.

--- test/integration/navbar_test.rb
+++ test/integration/navbar_test.rb
@@ -21,4 +21,38 @@ class NavbarTest < ActionDispatch::IntegrationTest
       assert_select 'a', text: 'Logout', href: logout_path
     end
   end
+
+  test 'navbar should have Upload Receipt link when logged in' do
+    sign_in confirmed_user
+    get root_path
+
+    assert_select 'nav' do
+      assert_select 'a', text: 'Upload Receipt', href: new_receipt_path
+    end
+  end
+
+  test 'navbar should not have Upload Receipt link when logged in' do
+    get root_path
+
+    assert_select 'nav' do
+      assert_select 'a', text: 'My Receipts', href: new_receipt_path, count: 0
+    end
+  end
+
+  test 'navbar should have My Receipts link when logged in' do
+    sign_in confirmed_user
+    get root_path
+
+    assert_select 'nav' do
+      assert_select 'a', text: 'My Receipts', href: receipts_path
+    end
+  end
+
+  test 'navbar should not have My Receipts link when logged in' do
+    get root_path
+
+    assert_select 'nav' do
+      assert_select 'a', text: 'My Receipts', href: receipts_path, count: 0
+    end
+  end
 end
