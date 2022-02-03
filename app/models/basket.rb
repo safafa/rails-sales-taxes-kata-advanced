e2a1a79cd01c6251cbf6b7557b4e3dd67ab17cdf
Safa ABALLAGH <aballaghsafa97@gmail.com>
2022-02-03 17:29:47 +0100

    Add build_entries_from_file

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,3 +1,15 @@
 class Basket < ApplicationRecord
-    has_many :entries, dependent: :destroy
+  has_many :entries, dependent: :destroy
+
+  def build_entries_from_file_upload(file)
+    lines = file.read.split("\n")
+    lines.each do |line|
+      info = line.split
+      quantity = info[0]
+      price = info[-1]
+      name = info[1..-3].join(' ')
+      entry = entries.build(name: name, price: price, quantity: quantity, basic_tax: 0, import_tax: 0)
+      entry.save
+    end
+  end
 end
