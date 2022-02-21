9ede0bd3eae56a967353f03969b6fd9f907df39a
safa aballagh <aballaghsafa@gmail.com>
2022-02-21 13:09:46 +0100

    Skip authentication in static_page_controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:home]
+  before_action :authenticate_user!
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def home
     @image_url = CatApi.img_url
   end
